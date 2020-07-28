30ac1c3b46997ee6e230361cc651fe92d6c9bd21
JauChin <jau.chin.shieh@gmail.com>
2020-07-28 14:20:13 +0800

    modify port setting

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 let express = require('express');
 let app = express();
-let get_time = function (req, res, next) {
-    console.log('Time:', Date.now());
-    next();
-};
-app.use(get_time);
+let port = process.env.PORT || 8888;
+// let get_time = function (req, res, next) {
+//     console.log('Time:', Date.now());
+//     next();
+// };
+// app.use(get_time);
 app.get("/", (req, res) => {
-    res.send("/");
+    res.send("/Hello world");
 });
 app.get("/about", (req, res) => {
     res.send("/about");
 });
 
-app.listen("8888", ()=>{
+app.listen(port, ()=>{
     console.log("we are listening at 8888");
 })
